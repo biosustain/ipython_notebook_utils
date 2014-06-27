580700f026fd75994aeccb1a82e6f5ae7a5b9c3b
Joao Cardoso <joaca@biosustain.dtu.dk>
2014-06-27 17:31:27 +0200

    initial commit

---
+++ ipython_notebook_utils/__init__.py
@@ -0,0 +1,44 @@
+# Copyright 2014 Novo Nordisk Foundation Center for Biosustainability, DTU.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import uuid
+from IPython.display import HTML, Javascript, display
+
+
+class ProgressBar():
+    def __init__(self, size=100):
+        self.progress = 0
+        self.size = size
+        self.id = "progress-bar-%s" % str(uuid.uuid4())
+
+    def start(self, width='50%'):
+        style = "width:%s;" % w idth
+        html = HTML("<progress id='%s' value='0' max='%i' style='%s'></progress>" % (self.id, self.size, style))
+        display(html)
+
+    def increment(self, i=1):
+        p = self.progress + i
+        self._update(p)
+
+    def set(self, progress):
+        p = progress
+        self._update(p)
+
+    def _update(self, p):
+        if p <= self.size:
+            js = Javascript("jQuery('#%s').val('%i')" % (self.id, p))
+            display(js)
+            self.progress = p
+        else:
+            raise RuntimeError("Already reached 100%")
---
+++ setup.py
@@ -0,0 +1,43 @@
+# Copyright 2013 Novo Nordisk Foundation Center for Biosustainability,
+# Technical University of Denmark.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+from setuptools import setup, find_packages
+from optlang import __version__
+
+with open('requirements.txt') as fhandle:
+    requirements = [line.strip() for line in fhandle]
+
+
+setup(
+    name='ipython_notebook_utils',
+    version=__version__,
+    packages=find_packages(),
+    install_requires=requirements,  # from requirements.txt
+    author='Joao Cardoso',
+    author_email='joaca@biosustain.dtu.dk',
+    description='IPython Notebook GUI utils',
+    license='Apache License Version 2.0',
+    keywords='ipython notebook gui utils',
+    url='TBD',
+    long_description="TODO",
+    classifiers=[
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
